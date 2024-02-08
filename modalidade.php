04d3444f0068e1be5bf2ae5e98b7f87e27f65984
Fontes21 <gabriel-santos619@hotmail.com>
2024-02-08 11:07:24 -0300

    init

---
+++ modalidade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+    <?php
+ 
+        include_once("class/modalidade.php");
+        $item = new modalidade();
+ 
+            $lista = $item->listarItem();
+ 
+            if ($lista != 0)
+            {
+             
+                foreach($lista as $i)
+                {
+ 
+                    $caminho = $i["caminho"];
+                    $titulo = $i["titulo"];
+                    $descricao = $i["descricao"];
+                   
+                    echo "
+                        <div class='card'>
+                            <img class='thumb' src='$caminho' alt='$titulo'>
+                            <p class='titulo-produto'>$titulo</p>
+                            <p>$descricao</p>
+                        </div>
+                    ";
+                }
+ 
+            }
+ 
+    ?>
+   
+</body>
+</html>
